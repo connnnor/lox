521b7e4d40bcf0542b01cb29c1f519a7dae440b7
kang <kang@localhost.localdomain>
2023-12-04 10:26:32 -0500

    fix switched < and > operators

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -73,10 +73,10 @@ public class Scanner {
                 addToken(match('=') ? EQUAL_EQUAL : EQUAL);
                 break;
             case '<':
-                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                addToken(match('=') ? LESS_EQUAL : LESS);
                 break;
             case '>':
-                addToken(match('=') ? LESS_EQUAL : LESS);
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
             // longer lexemes
             case '/':
