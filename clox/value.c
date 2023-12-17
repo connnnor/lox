b41e7935669cefb557c53f8853c7f1487218450a
kang <connor.r.kelleher@gmail.com>
2023-12-17 13:31:26 -0500

    end of ch14 chunks of bytecode

--- clox/value.c
+++ clox/value.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+#include "memory.h"
 #include "value.h"
+
+void init_value_arr(value_arr_t *arr) {
+  arr->values = NULL;
+  arr->capacity = 0;
+  arr->count = 0;
+}
+
+void write_value_arr(value_arr_t *arr, value_t value) {
+  if (arr->capacity < arr->count + 1) {
+    int old_capacity = arr->capacity;
+    arr->capacity = GROW_CAPACITY(old_capacity);
+    arr->values = GROW_ARRAY(value_t, arr->values, old_capacity, arr->capacity);
+  }
+  arr->values[arr->count] = value;
+  arr->count++;
+}
+
+void free_value_arr(value_arr_t *arr) {
+  FREE_ARRAY(value_t, arr->values, arr->capacity);
+  init_value_arr(arr);
+}
+void print_value(value_t value) {
+  printf("%g", value);
+}
--- clox/value.h
+++ clox/value.h
@@ -1,4 +1,17 @@
 #ifndef CLOX_VALUE_H
 #define CLOX_VALUE_H
 
+typedef double value_t;
+
+typedef struct {
+  int capacity;
+  int count;
+  value_t *values;
+} value_arr_t;
+
+void init_value_arr(value_arr_t *arr);
+void write_value_arr(value_arr_t *arr, value_t value);
+void free_value_arr(value_arr_t *arr);
+void print_value(value_t value);
+
 #endif //CLOX_VALUE_H
